392052e2c5b880ab3383f5dde9def0e781453314
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-11 22:27:39 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,8 @@
 def key_for_min_value(name_hash)
   lowest = nil
   name = nil
-  name_hash.each do |k, v|  
-    if (lowest ||= v) > v
+  name_hash.each do |k, v|
+    if (lowest ||= v) >= v
       lowest = v
       name = k
     end
